f2bfa02ea161f136f8dfc8b1f161d8f9278a681c
qzc_site <616042898@qq.com>
2022-07-27 14:36:24 +0800

    新增获取路径

--- core/utility.py
+++ core/utility.py
@@ -5,6 +5,8 @@
 # @software:PyCharm
 
 # qt常用包
+from PyQt5 import QtCore, QtGui, QtWidgets
+
 from PyQt5.QtWidgets import(
     QApplication,
     QWidget,
@@ -24,7 +26,15 @@ from PyQt5.QtWidgets import(
     QHBoxLayout,
     QVBoxLayout,
     QComboBox,
-    QScrollArea
+    QScrollArea,
+    QLineEdit,
+    QCompleter,
+    QListWidgetItem,
+    QGridLayout,
+    QTextEdit,
+    QListWidget,
+    QSpacerItem,
+    QSizePolicy,
 )
 from PyQt5.Qt import (
     QThread,
@@ -38,11 +48,12 @@ from PyQt5.QtGui import (
     QCursor
 )
 from PyQt5.QtCore import (
-    Qt as coreQt,
+    Qt as core_Qt,
     QPoint
 )
 
 # python 内置库
+import os
 import sys
 import time
 from datetime import datetime as dt
@@ -56,3 +67,14 @@ PersonalInfo = SMJPersonalInfo()
 # 让数据只导入一次
 def Mysql_PersonalInfo()->SMJPersonalInfo:
     return PersonalInfo
+
+
+# 根路径,无论从那个文件运行,要保证文件路径是一样的
+def rootPath()->str:
+    z_path= os.getcwd().split("scriptmanagement")
+    return os.path.join(z_path[0],"scriptmanagement")
+
+
+# 组合路径
+def joinPath(path:str)->str:
+    return os.path.join(rootPath(),path)
